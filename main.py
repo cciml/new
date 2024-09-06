c800083d3ba5d648654bd92383d0349e3581de27
cciml <94281564+cciml@users.noreply.github.com>
2024-09-06 22:29:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -141,7 +141,7 @@ def send_message(to_user, access_token, city_name, weather, max_temperature, min
         "url": "http://weixin.qq.com/download",
         "topcolor": "#FF0000",
         "data": {
-            "date": {
+            "every_day": {
                 "value": "{} {}".format(today, week),
                 "color": get_color()
             },
@@ -162,7 +162,7 @@ def send_message(to_user, access_token, city_name, weather, max_temperature, min
                 "color": get_color()
             },
             "love_day": {
-                "value": "{} {}".format(today, week),
+                "value": birthday_data,
                 "color": get_color()
             },
             "note_en": {
